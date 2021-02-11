9625f46ad292549609c32292a8d953f5c42f3510
Justin Szaro <justinszaro@gmail.com>
2021-02-11 11:02:34 -0500

    Add my name to Team4.java

--- Project00/src/Team4.java
+++ Project00/src/Team4.java
@@ -17,6 +17,7 @@ public class Team4 extends Team
     public void createTeamMembers()
     {
         members.add(new TeamMember("Maxwell Schuman"));
+	members.add(new TeamMember("Justin Szaro"));
 
         // create a new member class for each team member
         // and add it to the team members list
